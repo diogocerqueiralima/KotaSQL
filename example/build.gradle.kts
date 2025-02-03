1ac21968adbc48e8a2d1f6e1366e03467bf384cd
Diogo Lima <diogocerqueiradeelima@gmail.com>
2025-02-03 11:35:41 +0000

    feat: add module example

---
+++ example/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    alias(libs.plugins.ksp)
+}
+
+group = "com.github.diogocerqueiralima"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":processor"))
+    ksp(project(":processor"))
+}
+
+kotlin {
+    jvmToolchain(23)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ plugins {
 }
 rootProject.name = "KotaSQL"
 include("processor")
+include("example")
